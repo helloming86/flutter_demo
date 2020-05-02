ab3a968f44f8f6a2210a16a3b42bd82fc75cc2b2
Nicky <helloming86@gmail.com>
2020-05-02 11:47:15 +0800

    [feat]Flutter布局

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_demo/demo/less_group_page.dart';
-import 'package:flutter_demo/demo/plugin_use.dart';
-import 'package:flutter_demo/demo/stateful_group_page.dart';
+import 'package:flutter_demo/demo/flutter_layout_page.dart';
 
-void main() => runApp(StatefulGroup());
+void main() => runApp(FlutterLayoutPage());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
