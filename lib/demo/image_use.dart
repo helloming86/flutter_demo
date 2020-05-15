6fd6c98210455c3bb999fd34546b34e321c9c819
Nicky <helloming86@gmail.com>
2020-05-15 11:37:08 +0800

    [feat]Image Widget 的使用

---
+++ lib/demo/image_use.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:transparent_image/transparent_image.dart';
+//import 'package:cached_network_image/cached_network_image.dart';
+
+class ImageUse extends StatefulWidget {
+  @override
+  _ImageUseState createState() => _ImageUseState();
+}
+
+class _ImageUseState extends State<ImageUse> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Image"),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Icon(Icons.arrow_back),
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Image.network('http://www.devio.org/img/avatar.png'), // 加载网络图片
+          Image.asset(
+            'images/type_shop.png',
+            height: 26,
+            width: 26,
+          ), // 加载静态图片
+//          Image.file(File('/sdcard/Download/Stack.png')), // 加载本地图片 绝对路径
+          FutureBuilder(
+            future: _getLocalFile('/sdcard/Download/Stack.png'),
+            builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
+              return snapshot.data != null
+                  ? Image.file(snapshot.data)
+                  : Container(
+                      child: Text('找不到本地图片'),
+                    );
+            },
+          ), // 加载本地图片 相对路径 使用 path_provider
+          FadeInImage.memoryNetwork(
+            placeholder: kTransparentImage,
+            image: 'http://www.devio.org/img/avatar.png',
+          ), // 内存加载图片 transparent_image 加载placeholder
+//          CachedNetworkImage(
+//            imageUrl: 'https://picsum.photos/250?image=9',
+//            placeholder: (context, url) => CircularProgressIndicator(),
+//            errorWidget: (context, url, error) => Icon(Icons.error),
+//          ), // cached_network_image 缓存网络图片, iOS上会编译出错
+        ],
+      ),
+    );
+  }
+
+  Future<File> _getLocalFile(String filename) async {
+    String dir = (await getExternalStorageDirectory()).path;
+    File f = File('$dir/$filename');
+    return f;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_demo/demo/app_lifecircle_page.dart';
 import 'package:flutter_demo/demo/flutter_layout_page.dart';
 import 'package:flutter_demo/demo/gesture_page.dart';
+import 'package:flutter_demo/demo/image_use.dart';
 import 'package:flutter_demo/demo/launch_page.dart';
 import 'package:flutter_demo/demo/less_group_page.dart';
 import 'package:flutter_demo/demo/life_circle_page.dart';
@@ -37,14 +38,17 @@ class _DynamicThemeState extends State<DynamicTheme> {
             RaisedButton(
               onPressed: () {
                 setState(() {
-                  if(_brightness==Brightness.dark) {
+                  if (_brightness == Brightness.dark) {
                     _brightness = Brightness.light;
                   } else {
                     _brightness = Brightness.dark;
                   }
                 });
               },
-              child: Text("切换主题ABC", style: TextStyle(fontFamily: "Montserrat"),), // 局部更换字体
+              child: Text(
+                "切换主题ABC",
+                style: TextStyle(fontFamily: "Montserrat"),
+              ), // 局部更换字体
             ),
             RouteNavigator(),
           ],
@@ -60,38 +64,7 @@ class _DynamicThemeState extends State<DynamicTheme> {
         "launch": (BuildContext context) => LaunchPage(),
         "circle": (BuildContext context) => LifeCirclePage(),
         "appCircle": (BuildContext context) => AppLifeCirclePage(),
-      },
-    );
-  }
-
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("路由与导航"),
-        ),
-        body: RouteNavigator(),
-      ),
-      routes: <String, WidgetBuilder>{
-        "less": (BuildContext context) => LessGroupPage(),
-        "ful": (BuildContext context) => StatefulGroupPage(),
-        "plugin": (BuildContext context) => PluginUsePage(),
-        "layout": (BuildContext context) => FlutterLayoutPage(),
-        "gesture": (BuildContext context) => GesturePage(),
-        "res": (BuildContext context) => ResPage(),
-        "launch": (BuildContext context) => LaunchPage(),
-        "circle": (BuildContext context) => LifeCirclePage(),
-        "appCircle": (BuildContext context) => AppLifeCirclePage(),
-        "photoApp": (BuildContext context) => PhotoAppPage(),
+        "image": (BuildContext context) => ImageUse(),
       },
     );
   }
@@ -107,27 +80,36 @@ class _RouteNavigatorState extends State<RouteNavigator> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        children: <Widget>[
-          SwitchListTile(
-            title: Text("${byName?'':'不'}通过路由跳转"),
-              value: byName,
-              onChanged: (value) {
-                setState(() {
-                  byName = value;
-                });
-              }),
-          _item("StatefulWidget与基础组件", StatefulGroupPage(), "ful"),
-          _item("StateLessWidget与基础组件", LessGroupPage(), "less"),
-          _item("Flutter布局开发", FlutterLayoutPage(), "layout"),
-          _item("如何使用Flutter插件和包", PluginUsePage(), "plugin"),
-          _item("检测手势及点击处理", GesturePage(), "gesture"),
-          _item("Flutter导入使用资源文件", ResPage(), "res"),
-          _item("打开第三方应用", LaunchPage(), "launch"),
-          _item("页面声明周期", LifeCirclePage(), "circle"),
-          _item("APP声明周期", AppLifeCirclePage(), "appCircle"),
-          _item("拍照APP", PhotoAppPage(), "photoApp"),
-        ],
+      child: Expanded(
+        child: Column(
+          children: <Widget>[
+            SwitchListTile(
+                title: Text("${byName ? '' : '不'}通过路由跳转"),
+                value: byName,
+                onChanged: (value) {
+                  setState(() {
+                    byName = value;
+                  });
+                }),
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  _item("StatefulWidget与基础组件", StatefulGroupPage(), "ful"),
+                  _item("StateLessWidget与基础组件", LessGroupPage(), "less"),
+                  _item("Flutter布局开发", FlutterLayoutPage(), "layout"),
+                  _item("如何使用Flutter插件和包", PluginUsePage(), "plugin"),
+                  _item("检测手势及点击处理", GesturePage(), "gesture"),
+                  _item("Flutter导入使用资源文件", ResPage(), "res"),
+                  _item("打开第三方应用", LaunchPage(), "launch"),
+                  _item("页面声明周期", LifeCirclePage(), "circle"),
+                  _item("APP声明周期", AppLifeCirclePage(), "appCircle"),
+                  _item("拍照APP", PhotoAppPage(), "photoApp"),
+                  _item("图片的使用", ImageUse(), "image"),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -136,10 +118,11 @@ class _RouteNavigatorState extends State<RouteNavigator> {
     return Container(
       child: RaisedButton(
         onPressed: () {
-          if(byName){
+          if (byName) {
             Navigator.pushNamed(context, routeName);
           } else {
-             Navigator.push(context, MaterialPageRoute(builder: (context) => page));
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => page));
           }
         },
         child: Text(title),
